dd67f3767a9286449ed42d96cfaccaa0e16ef97d
Anthony <Kain235@gmail.com>
2022-05-03 14:23:35 -0400

    commit tree 944da3778dc9ab69d02988889dbe5df9f1d4fa62

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function handleError(error) {
+  document.querySelector('div').classList.remove('hidden')
+  document.querySelector('h2').innerText = error
+  setTimeout(() => document.querySelector('div').classList.add('hidden'), 3000)
+}
 
+function handleHeart(e) {
+  e.target.classList.toggle('activated-heart')
+}
+
+function doThings(e) {
+  mimicServerCall().then(() => handleHeart(e)).catch(error => handleError(error))
+}
+
+let likes = document.querySelectorAll(".like")
+for (const item of likes) {
+  item.addEventListener('click', e => doThings(e))
+
+}
 
 
 
@@ -11,9 +29,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
